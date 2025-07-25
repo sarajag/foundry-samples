96ee1b9daafb48634c67269d47f622bafb0e6c30
Dennis Eikelenboom <92387505+deeikele@users.noreply.github.com>
2025-07-25 10:53:51 -0700

    Add Foundry basic terraform sample (#267)
    * basic terraform sample
    
    * remove sub id

---
+++ samples/microsoft/infrastructure-setup-terraform/00-basic/code/main.tf
@@ -0,0 +1,99 @@
+## Create a random string
+## 
+resource "random_string" "unique" {
+  length      = 4
+  min_numeric = 4
+  numeric     = true
+  special     = false
+  lower       = true
+  upper       = false
+}
+
+## Create a resource group for the resources to be stored in
+##
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aifoundry${random_string.unique.result}"
+  location = var.location
+}
+
+########## Create AI Foundry resource
+##########
+
+## Create the AI Foundry resource
+##
+resource "azapi_resource" "ai_foundry" {
+
+  type                      = "Microsoft.CognitiveServices/accounts@2025-04-01-preview"
+  name                      = "aifoundry${random_string.unique.result}"
+  parent_id                 = azurerm_resource_group.rg.id
+  location                  = var.location
+  schema_validation_enabled = false
+
+  body = {
+    
+    kind = "AIServices"
+    sku = {
+      name = "S0"
+    }
+    identity = {
+      type = "SystemAssigned"
+    }
+
+    properties = {
+      # Support both Entra ID and API Key authentication for Cognitive Services account
+      disableLocalAuth = false
+
+      # Specifies that this is an AI Foundry resourceyes
+      allowProjectManagement = true
+
+      # Set custom subdomain name for DNS names created for this Foundry resource
+      customSubDomainName    = "aifoundry${random_string.unique.result}"
+    }
+  }
+}
+
+## Create a deployment for OpenAI's GPT-4o in the AI Foundry resource
+##
+resource "azurerm_cognitive_deployment" "aifoundry_deployment_gpt_4o" {
+  depends_on = [
+    azapi_resource.ai_foundry
+  ]
+
+  name = "gpt-4o"
+  cognitive_account_id = azapi_resource.ai_foundry.id
+
+  sku {
+    name     = "GlobalStandard"
+    capacity = 1
+  }
+
+  model {
+    format  = "OpenAI"
+    name    = "gpt-4o"
+    version = "2024-05-13"
+  }
+}
+
+## Create AI Foundry project
+##
+resource "azapi_resource" "ai_foundry_project" {
+  type                      = "Microsoft.CognitiveServices/accounts/projects@2025-04-01-preview"
+  name                      = "project${random_string.unique.result}"
+  parent_id                 = azapi_resource.ai_foundry.id
+  location                  = var.location
+  schema_validation_enabled = false
+
+  body = {
+    sku = {
+      name = "S0"
+    }
+    identity = {
+      type = "SystemAssigned"
+    }
+
+    properties = {
+      displayName = "project"
+      description = "My first project"
+    }
+  }
+}
---
+++ samples/microsoft/infrastructure-setup-terraform/00-basic/code/variables.tf
@@ -0,0 +1,4 @@
+variable "location" {
+  description = "The name of the location to provision the resources to"
+  type        = string
+}
